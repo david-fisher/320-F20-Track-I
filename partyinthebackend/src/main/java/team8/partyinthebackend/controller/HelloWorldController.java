377950ccce8a915fbf20622c1bb049fed458aa8a
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2020-09-30 10:23:39 -0400

    modify the dic use

--- partyinthebackend/src/main/java/team8/partyinthebackend/controller/HelloWorldController.java
+++ partyinthebackend/src/main/java/team8/partyinthebackend/controller/HelloWorldController.java
@@ -1,18 +1,13 @@
-package team8.partyinthebackend;
+package team8.partyinthebackend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * @Author: ZJendex
- * @Time: 9/26/2020, Sat
- **/
-
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-public class Routes {
+public class HelloWorldController {
     @RequestMapping("/")
     public String index(){
         return "Hello World";
