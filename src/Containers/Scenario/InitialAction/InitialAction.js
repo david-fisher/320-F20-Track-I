f6b7ed43e9505881ef769fb89e970ff6e185e891
stephendisario <sdisario@umass.edu>
2020-10-15 11:45:14 -0400

    small change

--- src/Containers/Scenario/InitialAction/InitialAction.js
+++ src/Containers/Scenario/InitialAction/InitialAction.js
@@ -2,7 +2,6 @@ import React, {useState,useEffect} from 'react';
 import { Row, Col, Typography, Button, Radio } from 'antd';
 import { useHistory } from "react-router-dom";
 import Navbar from '../../../component/dashboarditems/Navbar'
-import useSelection from 'antd/lib/table/hooks/useSelection';
 
 const InitialAction = (props) => {
   
@@ -42,40 +41,6 @@ const InitialAction = (props) => {
       <Row>
           <Col offset={5}><Button type="primary" htmlType="submit" style={{marginTop: "10px"}} onClick={() => {history.push("/scenario/gather-information")}}>Next Page</Button></Col>
       </Row>
-
-    {/* <Row gutter={16}>
-        <ul className="scenarioIntro-headermargin"> </ul>
-          <Card bordered={false} style={{textAlign:"center", fontSize:26}}> 
-            <p>
-              Choose initial action:
-            </p>
-          </Card>
-    </Row>
-
-    <Row justify="center" style={{ marginBottom: "10px" }}>
-      <Card style={{textAlign:"left", fontSize:16,width:"55rem",height:"30rem",background:"#ECECEC" }}> 
-      <p>
-      You're a new employee working on a data science team that is working on a larger project. You've also had an oportunity to reflect on your understanding of the task that you have been assigned on this project and to list any questions that still remain for you.
-      </p>
-      <p>
-      Please select what you would like to do next
-      </p>
-      <p>
-      Would you:
-      </p>
-      <Col><Button type="text" htmlType="submit" style={{ fontSize:16}} size = "middle" 
-        onClick={() => {
-          history.push("/scenario/gather-information")}
-          }> <u>get to work so that you don't feel stressed about completing your task assignment before the project deadline</u></Button></Col> 
-      <p>
-      Or
-      </p>
-      <Col><Button type="text" htmlType="submit" style={{ fontSize:16}}  size = "small"
-        onClick={() => {
-          history.push("/scenario/gather-information")}
-          }><u>delay getting started and try to get answers for any lingering questions that you might have about the project</u></Button></Col> 
-      </Card>
-    </Row> */}
 
     </>
   );
