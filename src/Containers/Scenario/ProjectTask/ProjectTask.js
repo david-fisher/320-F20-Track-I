570bd0d37fd0eefd62156497c78975f528c6104f
jchanimal <jeffreychan3820@gmail.com>
2020-10-29 09:27:35 -0400

    projectTask page with Card from antd

--- src/Containers/Scenario/ProjectTask/ProjectTask.js
+++ src/Containers/Scenario/ProjectTask/ProjectTask.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect} from 'react';
-import { Row, Col, Typography, Button } from 'antd';
+import { Row, Col, Typography, Button, Card } from 'antd';
 import { useHistory } from "react-router-dom";
 import {SidebarContext} from "../../../component/SidebarContext"
 import HelpMenu from '../../../Containers/Scenario/Conversations/helpMenu';
@@ -33,42 +33,49 @@ const ProjectTask = () => {
   return (
     <>
       <Row>
+        
         <Col offset={5} span={18}>
+        <Card style={{textAlign:"left", fontSize:16,width:"50rem",height:"50rem",background:"#ECECEC"}}> 
             <Title style={{color: "black"}}>Project Task</Title>
-            <Text strong> 1. This is Task 1</Text><br/>
-            <ul style={{marginLeft:"40px"}}>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-            </ul>
-            <Text strong> 2. This is Task 2</Text><br/>
-            <ul style={{marginLeft:"40px"}}>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-            </ul>
-            <Text strong> 3. This is Task 3</Text><br/>
-            <ol style={{marginLeft:"40px"}}>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-              <li><Text>this is text</Text></li>
-            </ol>
+            <Text>We want to scape data from out own website and from outside sources to sharpen our predictive 
+            ML model for cognitive decline. The data scarping
+            tool might colect the following additional data for potentail new features of the model:</Text>
             <br/>
-            <Paragraph>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-              It has survived not only five centuries, but also the leap into electronic typesetting, 
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-              sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-              Aldus PageMaker including versions of Lorem Ipsum.
+            <Text strong> 1. My Medical Advisor website user interactions</Text><br/>
+            <ul style={{marginLeft:"40px"}}>
+              <li><Text>Typing speed</Text></li>
+              <li><Text>Spelling erros</Text></li>
+              <li><Text>Rate of Typing errors</Text></li>
+              <li><Text>Incidence of repetitive requests</Text></li>
+              <li><Text>Reading speed</Text></li>
+            </ul>
+            <Text strong> 2. Users' social media content</Text><br/>
+            <ul style={{marginLeft:"40px"}}>
+              <li><Text>Posts by and about the user</Text></li>
+              <li><Text>Pictures and videos</Text></li>
+              <li><Text>Relationships: Family, significant other, friends</Text></li>
+              <li><Text>Hobbies, exercise, and other activities</Text></li>
+            </ul>
+            <Text strong> 3. Premiums and memberships</Text><br/>
+            <ul style={{marginLeft:"40px"}}>
+              <li><Text>Loyaly card purchases</Text></li>
+              <li><Text>Browser histories</Text></li>
+              <li><Text>Email</Text></li>
+            </ul>
+            <br/>
+            <Paragraph>Part of your assignment is to identify specific companies who would be willing to provide data and also
+            make recommendations for further data to collect, in order to refine the above list. Once the data is in hand,
+            you will use it to improve the existing predictive model for cognitive decline, by incorporating new training
+            features as appropriate.
+            
           </Paragraph>
+          <Button type="primary" htmlType="submit" onClick={handleClick}>Next Page</Button>
+          <Col offset={23}><HelpMenu/></Col>
+          </Card>
         </Col>
       </Row>
       <Row>
-          <Col offset={5}><Button type="primary" htmlType="submit" onClick={handleClick}>Next Page</Button></Col><Col offset={23}><HelpMenu/></Col>
+          <Col offset={5}></Col>
       </Row>
     </>
   );
