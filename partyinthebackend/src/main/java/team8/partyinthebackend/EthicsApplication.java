29aaf381c3ab18d2a413fddb5e980b89ef7caddb
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2020-10-30 21:57:33 -0400

    get rid of the database part of the app

--- partyinthebackend/src/main/java/team8/partyinthebackend/EthicsApplication.java
+++ partyinthebackend/src/main/java/team8/partyinthebackend/EthicsApplication.java
@@ -2,8 +2,10 @@ package team8.partyinthebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EthicsApplication {
 
 	public static void main(String[] args) {
