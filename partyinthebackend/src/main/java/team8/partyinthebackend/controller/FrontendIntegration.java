85a53bbe8d555864005a3539da9307bdd8bb03f3
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2020-11-04 17:47:49 -0500

    Add tow more Get
    project task assignment & Gather infomation

--- partyinthebackend/src/main/java/team8/partyinthebackend/controller/FrontendIntegration.java
+++ partyinthebackend/src/main/java/team8/partyinthebackend/controller/FrontendIntegration.java
@@ -78,6 +78,50 @@ public class FrontendIntegration {
             return obj;
         }
     }
+
+    /**
+     * (GET) project task assignment
+     */
+
+    @GetMapping(value = "/scenario/{scenario_id}/{version_id}/pta")
+    public JSONObject getPTA(@PathVariable int scenario_id, @PathVariable int version_id){
+        try {
+            JSONObject rst = new JSONObject();
+            rst.put("text", "Ultrices gravida dictum fusce ut. At lectus urna duis convallis convallis tellus id interdum. Faucibus in ornare quam viverra orci. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. Pellentesque elit eget gravida cum sociis natoque. Aliquet eget sit amet tellus cras adipiscing enim. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Orci nulla pellentesque dignissim enim sit amet.\n" + "\n" + "Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. ");
+            rst.put("page_title", "Project Task Assignment Page");
+            JSONObject obj = new JSONObject();
+            obj.put("body", rst);
+            obj.put("status_code", 200);
+            return obj;
+        }
+        catch(Exception e) {
+            JSONObject obj = new JSONObject();
+            obj.put("status_code", 404);
+            return obj;
+        }
+    }
+
+    /**
+     * (GET) gather information
+     */
+
+    @GetMapping(value = "/scenario/{scenario_id}/{version_id}/gi")
+    public JSONObject getGI(@PathVariable int scenario_id, @PathVariable int version_id){
+        try {
+            JSONObject rst = new JSONObject();
+            rst.put("text", "Ultrices gravida dictum fusce ut. At lectus urna duis convallis convallis tellus id interdum. Faucibus in ornare quam viverra orci. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. Pellentesque elit eget gravida cum sociis natoque. Aliquet eget sit amet tellus cras adipiscing enim. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Orci nulla pellentesque dignissim enim sit amet.\n" + "\n" + "Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. ");
+            rst.put("page_title", "Gather Information Page");
+            JSONObject obj = new JSONObject();
+            obj.put("body", rst);
+            obj.put("status_code", 200);
+            return obj;
+        }
+        catch(Exception e) {
+            JSONObject obj = new JSONObject();
+            obj.put("status_code", 404);
+            return obj;
+        }
+    }
     
     @GetMapping(value = "/student/{student_id}/scenario/{scenario_id}/{version_id}/initialreflection")
     public JSONObject getInitialReflection(@PathVariable int student_id, @PathVariable int scenario_id, @PathVariable int version_id) {
@@ -99,7 +143,7 @@ public class FrontendIntegration {
      * (PUT) puts a students answer to the initial reflection in the student dummy object
      */
 
-    @PutMapping(value = "/student/{student_id}/scenario/{scenario_id}/{version_id}/initialreflection")
+    @PostMapping(value = "/student/{student_id}/scenario/{scenario_id}/{version_id}/initialreflection")
     public @ResponseBody JSONObject updateOneStudent(@PathVariable int student_id, @PathVariable int scenario_id, @PathVariable int version_id, @RequestParam String response) {
         try {
             JSONObject obj = new JSONObject();
