e1d856a5355ced5e3d538a3f3b41541c36dd430d
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2020-11-04 10:50:46 -0500

    Delete unrelated controller

---
+++ partyinthebackend/src/main/java/team8/partyinthebackend/controller/FrontendIntergration.java
@@ -0,0 +1,49 @@
+package team8.partyinthebackend.controller;
+
+import net.minidev.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+import team8.partyinthebackend.model.Page;
+import team8.partyinthebackend.model.Scenario;
+import team8.partyinthebackend.model.StakeholderList;
+import team8.partyinthebackend.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: ZJendex
+ * @Time: 10/28/2020, Wed
+ **/
+
+@RestController
+public class FrontendIntergration {
+
+    /**
+     * (GET) introduction
+     */
+
+    @GetMapping(value = "/scenario/{scenario_id}/{version_id}/introduction")
+    public JSONObject getIntroduction(@PathVariable int scenario_id, @PathVariable int version_id){
+        try {
+            JSONObject rst = new JSONObject();
+            rst.put("text", "Ultrices gravida dictum fusce ut. At lectus urna duis convallis convallis tellus id interdum. Faucibus in ornare quam viverra orci. Sit amet tellus cras adipiscing enim eu turpis egestas pretium. Pellentesque elit eget gravida cum sociis natoque. Aliquet eget sit amet tellus cras adipiscing enim. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Orci nulla pellentesque dignissim enim sit amet.\n" + "\n" + "Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. ");
+            rst.put("page_title", "Introduction Page");
+            JSONObject obj = new JSONObject();
+            obj.put("body", rst);
+            obj.put("status_code", 200);
+            return obj;
+        }
+        catch(Exception e) {
+            JSONObject obj = new JSONObject();
+            obj.put("status_code", 404);
+            return obj;
+        }
+    }
+
+
+//    @PutMapping(value = "/students/{id}")
+//    public @ResponseBody Student updateOneStudent(@PathVariable int id, @RequestParam String name) {
+//        Data.students.get(id-1).setName(name);
+//        return Data.students.get(id-1);
+//    }
+}
