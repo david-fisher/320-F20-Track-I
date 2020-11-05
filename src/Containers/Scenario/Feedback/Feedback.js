3e3bf6c70e06f331ce2ec03e70319bc90ae1821b
stephendisario <sdisario@umass.edu>
2020-11-05 09:49:05 -0500

    radar plot

--- src/Containers/Scenario/Feedback/Feedback.js
+++ src/Containers/Scenario/Feedback/Feedback.js
@@ -2,6 +2,8 @@ import React, {useContext,useEffect} from 'react';
 import { Row, Col, Typography, Divider, Input, Button } from 'antd';
 import { useHistory } from "react-router-dom";
 import {SidebarContext} from '../../../component/SidebarContext';
+import { Radar } from 'react-chartjs-2'
+
 
 const Feedback = () => {
 
@@ -10,6 +12,25 @@ const Feedback = () => {
     const {state,update} = useContext(SidebarContext)
 
     let history = useHistory();
+
+    const data = {
+      labels: ['Ethic Thing 1', 'Ethic Thing 2', 'Ethic Thing 3', 'Ethic Thing 4', 'Ethic Thing 5', 'Ethic Thing 6'],
+      datasets: [
+        {
+          label: '# of votes',
+          data: [1,1,3,2,3,2],
+          backgroundColor: 'rgba(255, 0, 0, 0.2)',
+          borderColor: 'rgba(255, 0, 0, 1)',
+          borderWidth: 1,
+        },
+      ],
+    }
+
+    const options = {
+      scale: {
+        ticks: { beginAtZero: true },
+      },
+    }
 
 
     const handleClick = () => {
@@ -28,6 +49,7 @@ const Feedback = () => {
             <Col offset={5} span={18}>
                 <Title style={{color: "black"}}>Feedback</Title>
                 <Title style={{color: "black"}}>There will be a radar plot here soon...</Title>
+                <Radar data={data} options={options} />
                 <Paragraph>Words about the plot<br/><br/>
                 </Paragraph>
                 <Divider />
