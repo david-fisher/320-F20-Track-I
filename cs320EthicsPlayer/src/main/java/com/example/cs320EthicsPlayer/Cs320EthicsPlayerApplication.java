a5388cb1887db7d81715ab2ca3831fb472e37749
Jiangyifei Zhu <47931455+ZJendex@users.noreply.github.com>
2020-11-29 09:33:18 -0500

    Update latest changes on DB

--- cs320EthicsPlayer/src/main/java/com/example/cs320EthicsPlayer/Cs320EthicsPlayerApplication.java
+++ cs320EthicsPlayer/src/main/java/com/example/cs320EthicsPlayer/Cs320EthicsPlayerApplication.java
@@ -2,8 +2,11 @@ package com.example.cs320EthicsPlayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.example.cs320EthicsPlayer.repository"})
+@ComponentScan(basePackages = { "com.example.cs320EthicsPlayer.model"})
 public class Cs320EthicsPlayerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Cs320EthicsPlayerApplication.class, args);
