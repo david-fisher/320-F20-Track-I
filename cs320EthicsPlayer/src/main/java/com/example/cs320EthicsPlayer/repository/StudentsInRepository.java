8ab4f01d7b65ea5a203aaa6f39f69b1167c23e6c
abigail432 <54914351+abigail432@users.noreply.github.com>
2020-11-29 20:22:25 -0500

    getCoursesOfStudent() and getStudentsInCourse() API call fixes

--- cs320EthicsPlayer/src/main/java/com/example/cs320EthicsPlayer/repository/StudentsInRepository.java
+++ cs320EthicsPlayer/src/main/java/com/example/cs320EthicsPlayer/repository/StudentsInRepository.java
@@ -8,6 +8,6 @@ import com.example.cs320EthicsPlayer.model.StudentsInID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentsInRepository extends JpaRepository<StudentsIn, StudentsInID> {
-    List<Integer> findBycourseID(int c_ID);
-    List<Integer> findBystudentID(int s_ID);
+    List<StudentsIn> findByCourseID(int c_ID);
+    List<StudentsIn> findByStudentID(int s_ID);
 }
