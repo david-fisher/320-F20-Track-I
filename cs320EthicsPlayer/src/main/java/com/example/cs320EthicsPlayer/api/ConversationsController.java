04d4e928775b0fdaa917973ffa462e5dc3d70abb
mk-murtagh <70217278+mk-murtagh@users.noreply.github.com>
2020-12-02 18:49:33 -0500

    specificstakeholders

--- cs320EthicsPlayer/src/main/java/com/example/cs320EthicsPlayer/api/ConversationsController.java
+++ cs320EthicsPlayer/src/main/java/com/example/cs320EthicsPlayer/api/ConversationsController.java
@@ -2,10 +2,13 @@ package com.example.cs320EthicsPlayer.api;
 
 import java.util.List;
 
+import com.example.cs320EthicsPlayer.model.Conversations;
+import com.example.cs320EthicsPlayer.model.ConversationsID;
 import com.example.cs320EthicsPlayer.repository.ConversationsRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +21,13 @@ public class ConversationsController {
 
     //getSpecificStakeholderConversations()
 
-    @GetMapping("/getSpecificStakeholdersConvos/{cID}/{scen}/{vers}")
-    public List<Integer> getSpecificStakeholderConversations(){
-        return null;
+    @GetMapping("/getSpecificStakeholdersConvos/{stk}/{convo}")
+    public Conversations getSpecificStakeholderConversations(@PathVariable(value="stk") int stk, @PathVariable(value="convo") int convo)
+        throws Exception{
+        ConversationsID a = new ConversationsID(stk, convo);
+        Conversations b = conversationsRepository.findById(a)
+            .orElseThrow(() -> new Exception("Conversation not found"));
+        return b;
     }
     
 }
